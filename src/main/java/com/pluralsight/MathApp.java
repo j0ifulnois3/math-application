829bc8a03b12d95f9ba04a899e2ca03566fbaab1
Joi McBrayer <j0ifulnois3@gmail.com>
2026-04-10 08:36:11 -0500

    feat: display first two steps

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -6,8 +6,10 @@ public class MathApp {
         int bobSalary = 42000;
         int garySalary = 35000;
         int highestSalary = 150000;
-        System.out.println("The highest salary is " + highestSalary
-        );
+        System.out.println("The highest salary is " + highestSalary);
+
+        int carPrice = 23475;
+        int truckPrice = 85976;
 
     }
 
