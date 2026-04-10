88a0c7020d039c18bf8b907872cb69b83056a563
Joi McBrayer <j0ifulnois3@gmail.com>
2026-04-10 08:32:08 -0500

    Initial Commit

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+
+        int bobSalary = 42000;
+        int garySalary = 35000;
+        int highestSalary = 150000;
+        System.out.println("The highest salary is " + highestSalary
+        );
+
+    }
+
+}
